67441ffb604de77c108c9a91f907c33537d6f56c
BenderMaster <franciscodeassissouza21@gmail.com>
2022-03-25 14:47:11 -0300

    Exercicio Fatorial

---
+++ src/com/dio/exercicios/loops/Ex6_Fatorial.java
@@ -0,0 +1,21 @@
+package com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe numero fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.print(fatorial + "! = ");
+        for (int i = fatorial; i >=1; i--){
+            multiplicacao *= i;
+        }
+
+        System.out.println(multiplicacao);
+    }
+}
