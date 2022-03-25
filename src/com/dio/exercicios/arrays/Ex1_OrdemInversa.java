63b245dda38efb3ba29f2c4a231861084a450cb5
BenderMaster <franciscodeassissouza21@gmail.com>
2022-03-25 19:57:39 -0300

    Exercicio Ordem inversa array

---
+++ src/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
@@ -0,0 +1,22 @@
+package com.dio.exercicios.arrays;
+
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+
+        int [] vetor = {-5, -6, 15, 50, 4, 8};
+
+        System.out.print("Vetor: ");
+        int count = 0;
+        while(count < (vetor.length)){
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nVetor: ");
+        for(int i = (vetor.length -1); i >= 0; i--){
+            System.out.print(vetor[i] + " ");
+
+        }
+
+    }
+}
